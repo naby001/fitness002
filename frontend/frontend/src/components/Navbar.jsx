9e94230df9842eb73f9381483338b002a6aa49e8
naby <nabyendutukai.das@gmail.com>
2025-01-12 19:00:24 +0530

    css fixed

--- frontend/frontend/src/components/Navbar.jsx
+++ frontend/frontend/src/components/Navbar.jsx
@@ -3,19 +3,7 @@ import { Link } from 'react-router-dom';
 const Navbar = () => {  
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-light fixed-left"> {/* Changed to fixed-left */}
-            <div className="container-fluid">
-                <Link className="navbar-brand" to="/">Workout Tracker</Link>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/">Home</Link>
-                        </li>
-                        <li className="nav-item">
-                            <Link className="nav-link" to="/new">New Workout</Link>
-                        </li>
-                    </ul>
-                </div>
-            </div>
+           <h1 className="text-center w-full">Workout Buddy</h1> {/* Centered using Tailwind CSS */}
         </nav>
     );
 };
