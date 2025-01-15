3fad57115ef5ef7eb20b5acb397e97aa3d4b96bf
naby <nabyendutukai.das@gmail.com>
2025-01-15 20:35:36 +0530

    HOme

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/home'; 
+import Home from './pages/Home'; 
 import './App.css';
-import Navbar from './components/navbar'; 
+import Navbar from './components/Navbar'; 
 
 
 
