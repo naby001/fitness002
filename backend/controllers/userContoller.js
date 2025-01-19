4c680905be478379adb18ad0289572299568f9a0
naby <nabyendutukai.das@gmail.com>
2025-01-19 14:24:46 +0530

    lec6

--- backend/controllers/userContoller.js
+++ backend/controllers/userContoller.js
@@ -1,26 +1,35 @@
-const User = require('../models/userModel');
+const User = require("../models/userModel");
+const jswt = require("jsonwebtoken");
+
+const createToken = (_id) => {
+  // Return the generated token
+  return jswt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
+};
 
 // Login user
 const loginuser = async (req, res) => {
-    res.json({ mssg: "login user" });
+  res.json({ mssg: "login user" });
 };
 
 // Signup user
 const signupuser = async (req, res) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    try {
-        // Attempt to create a user using the `signup` method of your User model
-        const user = await User.signup(email, password);
+  try {
+    // Attempt to create a user using the `signup` method of your User model
+    const user = await User.signup(email, password);
 
-        // Send success response
-        res.status(200).json({ email, user });
-    } catch (error) {
-        // Send error response
-        res.status(400).json({ error: error.message });
-    }
+    // Create a token
+    const token = createToken(user._id);
 
-    // Do NOT send another response after the try/catch blocks
+    // Send success response
+    res.status(200).json({ email, token });
+  } catch (error) {
+    // Send error response
+    res.status(400).json({ error: error.message });
+  }
+
+  // Do NOT send another response after the try/catch blocks
 };
 
 // Correct export statement
