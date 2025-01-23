874823385b8c53b9a00254bfb1c1ffe7a40a9893
naby <nabyendutukai.das@gmail.com>
2025-01-23 23:38:33 +0530

    13

--- frontend/frontend/src/components/Navbar.jsx
+++ frontend/frontend/src/components/Navbar.jsx
@@ -1,8 +1,10 @@
 import { Link } from "react-router-dom";
 import { useLogout } from "../Hooks/useLogout";
+import { useAuthContext } from "../Hooks/useAuthContext";
 
 const Navbar = () => {
   const {logout } =useLogout()
+  const {user}=useAuthContext()
 
   const handleClick=()=>{
     logout()
@@ -13,14 +15,19 @@ const Navbar = () => {
       <Link to="/">
       <h1 className="navbar-title">Workout Buddy</h1> 
       </Link>
+      {user&&(
+         
       <div>
+        
+        <span>{user?.email}</span>
         <button onClick={handleClick}>Log  Out</button>
       </div>
-      <div>
+      )}
+      {!user&&(<div>
         <Link to="/login">Login</Link>
         <Link to="/signup">Sign Up</Link>
 
-      </div>
+      </div>)}
     </nav>
   );
 };
--- frontend/frontend/src/context/AuthContext.jsx
+++ frontend/frontend/src/context/AuthContext.jsx
@@ -1,4 +1,4 @@
-import { createContext,useReducer } from "react";
+import { createContext,useReducer,useEffect } from "react";
 export const AuthContext=createContext()
 
 export const authReducer=(state,action)=>{
@@ -16,6 +16,14 @@ export const AuthContextProvider=({children})=>{
     const[state,dispatch] =useReducer(authReducer,{
         user:null
     })
+
+    useEffect(()=>{
+        const user=localStorage.getItem('user')
+        if(user){
+            dispatch({type:'LOGIN',payload:JSON.parse(user)})
+        }
+    },[])
+
 console.log('AuthContext state:',state)
 
 return(
