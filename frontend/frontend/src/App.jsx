64b7d5043f4eda90debb2e06d9d57bf9affa0201
naby <nabyendutukai.das@gmail.com>
2025-01-26 14:47:36 +0530

    16

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route,Navigate } from 'react-router-dom';
+import { useAuthContext } from './Hooks/useAuthContext';
 import Home from './pages/Home'; 
 import './App.css';
 import Navbar from './components/Navbar'; 
@@ -9,6 +10,7 @@ import Signup from './pages/Signup';
 
 
 function App() {
+  const {user}=useAuthContext();
   return (
     <>
       <BrowserRouter>
@@ -16,10 +18,10 @@ function App() {
        <Navbar/>
    
         <div className="pages">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
+          <Routes> 
+            <Route path="/" element={user?< Home />:<Navigate to ="/login"/>} />
+            <Route path="/login" element={!user? <Login /> :<Navigate to ="/"/>} />
+            <Route path="/signup" element={!user?<Signup />:<Navigate to ="/"/>} />
             
           </Routes>
         </div>
